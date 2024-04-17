3a9f3e83598320bce3e58f2982bdaba9d111f42d
guscarenci <gustavomourascarenci@usp.br>
2024-04-17 10:56:33 -0300

    📃Adciona descrição e organização básica de arquivo do código e makefile

---
+++ src/jacobeq.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello Wolrd! Jacobi Sequencial...");
+    return 0;
+}
---
+++ src/jacobpar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(){
+    printf("Hello Wolrd! Jacobi Paralelo...");
+    return 0;
+}
