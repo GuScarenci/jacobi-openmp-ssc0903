4fb2ec9c50c0e50400d12f6935898a8e80740b70
guscarenci <gustavomourascarenci@usp.br>
2024-04-27 18:18:08 -0300

    adds parallelism based on tasks

--- src/jacobipar.c
+++ src/jacobipar.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
 
-double* jacobipar(double* matrix, double* constants, int N, float errorTolerance, int maxIterations) {
-    double* lastVariables = malloc(sizeof(double) * N);
-    double* currentVariables = malloc(sizeof(double) * N);
-    if (!lastVariables || !currentVariables) {
-        free(lastVariables);
-        free(currentVariables);
-        return NULL; // Handle malloc failure
-    }
+double* jacobipar(double* matrix,double* constants,int N,float errorTolerance,int maxIterations){
 
-    #pragma omp simd
-    for (int i = 0; i < N; i++) {
+    double* lastVariables = malloc(sizeof(double)*N);
+    double* currentVariables = malloc(sizeof(double)*N);
+
+    #pragma omp simd //performs a little better than "parallel for" for tested cases
+    for(int i = 0;i<N;i++){
         lastVariables[i] = 0;
     }
 
     int counter = 0;
     int convergenceProved = 1;
 
+    double delta = omp_get_wtime();
     do {
         convergenceProved = 1;
-
-        #pragma omp parallel for shared(lastVariables, currentVariables, matrix, constants) reduction(&& : convergenceProved)
+        //#pragma omp parallel for shared(lastVariables, currentVariables, matrix, constants) reduction(&& : convergenceProved)
         for (int i = 0; i < N; i++) {
             double sum = 0;
 
@@ -59,7 +56,9 @@ double* jacobipar(double* matrix, double* constants, int N, float errorTolerance
 
         counter++;
     } while (!convergenceProved && counter < maxIterations);
+    delta = (omp_get_wtime() - delta)*1000;
+    printf("Middle time: %lf ms\n",delta);
 
     free(currentVariables);
-    return lastVariables; // Caller must free this
+    return lastVariables; //Free outside
 }
