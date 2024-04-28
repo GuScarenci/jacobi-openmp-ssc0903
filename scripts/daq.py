18b502d87ff249f0175ef98def4ec04b09010785
Matheus Cirillo <matheuscirillo@usp.br>
2024-04-28 20:37:38 -0300

    new workload and thread count

--- scripts/daq.py
+++ scripts/daq.py
@@ -125,9 +125,9 @@ def generate_csv(data, file_name='data.csv'):
     print(f"CSV file successfuly generated at {file_path}!")
 
 compile()
-sizes = [10, 100]
-threads = [2, 4]
+sizes = [1000, 10000, 100000, 1000000]
+threads = [2, 4, 8]
 runs = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 raw_data = run(runs, sizes, threads)
-data = interpret_data(raw_data)
+data = interpret_data(raw_data, show_data=True)
 generate_csv(data)
