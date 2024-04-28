d8f8341a285634bb4e25c70a6f66abcb6ab4900a
Matheus Cirillo <matheuscirillo@usp.br>
2024-04-28 17:51:46 -0300

    upgrade in makefile
    Makefile now generates two different builds
    Removed builds from git

--- src/main.c
+++ src/main.c
@@ -16,11 +16,9 @@ int main(int argc, char *argv[]){
 
     int N = atoi(argv[1]);
     int T = atoi(argv[2]);
-    int seed = atoi(argv[1]);
-    //FINISHES ARGUMENTS TREATMENT
-
-    //GENERATE MATRIX AND VECTORS
-    double delta = omp_get_wtime();
+    int seed = atoi(argv[3]);
+    double tolerance = 0.001;
+    int maxIterations = 10000;
 
     double* matrix = createMatrix(N);
     double *constants = createConstants(N);
@@ -28,77 +26,12 @@ int main(int argc, char *argv[]){
     srand(seed);
     omp_set_nested(1);
 
-    delta = (omp_get_wtime() - delta)*1000;
-    printf("IO time: %lf ms\n",delta);
-    //FINISH MATRIX AND VECTORS GENERATIONS
-    
-    //SEQUENTIAL JACOBI
-    delta = omp_get_wtime();
-    double *results = jacobiseq(matrix,constants,N,0.001,10000);
-    delta = (omp_get_wtime() - delta)*1000;
-    printf("Sequential time: %lf ms\n",delta);
-    //FINISH SEQUENTIAL JACOBI
+#ifdef JACOBIPAR
+    jacobipar(matrix,constants,N,tolerance,maxIterations);
+#else
+    jacobiseq(matrix,constants,N,tolerance,maxIterations);
+#endif
 
-    //PARALLEL JACOBI
-    double delta2 = omp_get_wtime();
-    double *results2 = jacobipar(matrix,constants,N,0.001,10000);
-    delta2 = (omp_get_wtime() - delta2)*1000;
-    printf("Parallel time: %lf ms\n",delta2);
-    //FINISH PARALLEL JACOBI
-
-    //CALCULATES SPEEDUP AND EFFICIENCY
-    printf("Speedup:%lf\n",(delta/delta2));
-    printf("Eficiência:%lf\n",(delta/delta2)/T);
-    //FINISHES SPEEDUP AND EFFICIENCY CALCULUS
-
-    //SHOWS EQUATION REQUESTED BY USER (WITH SEQUENTIAL AND PARALLEL)
-    printf("Qual equação deseja ver? Escolha um número de 0 até %d.\n",N-1);
-    int eqChoice = -1;
-    while(eqChoice<0 || eqChoice>=N){
-        scanf("%d",&eqChoice);
-        if(eqChoice<0 || eqChoice>=N){
-            printf("Escolha um número de 0 até %d!\n",N-1);
-        }
-    }
-
-    double temp = 0;
-    for(int j = 0;j<N;j++){
-        temp += matrix[eqChoice*N+j]*results[j];
-    }
-    printf("Resultado calculado pelo sequencial:%lf\n",temp);
-
-    temp = 0;
-    for(int j = 0;j<N;j++){
-        temp += matrix[eqChoice*N+j]*results2[j];
-    }
-
-    printf("Resultado calculado pelo paralelo:%lf\n",temp);
-
-    printf("Resultado esperado:%lf\n\n",constants[eqChoice]);
-    //END EQUATION REQUESTED BY USER SHOWING (WITH SEQUENTIAL AND PARALLEL)
-
-
-    //CHECKS IF BOTH METHODS GOT THE SAME RESULT
-    vectorCompare(results,results2,N);
-    //END IF BOTH METHODS GOT THE SAME RESULT CHECKING
-    
-    //FREE POINTERS
-    free(results);
-    free(results2);
     free(matrix);
     free(constants);
-    //END POINTERS FREEING
 }
-
-//EXAMPLE OF MATRIX TO CHECK IF JACOBI IS WORKING! (only because i know the right result for these inputs)
-// double matrix2[3][3] = {{3,-1,-1},{-1,3,1},{2,1,4}};
-// for(int i = 0;i<N;i++){
-//     for(int j =0;j<N;j++){
-//         matrix[i*N + j] = matrix2[i][j];
-//     }
-// }
-// double constants2[3] = {1,3,7};
-// for(int i = 0;i<N;i++){
-//     constants[i] = constants2[i];
-// }
-//EXAMPLE
