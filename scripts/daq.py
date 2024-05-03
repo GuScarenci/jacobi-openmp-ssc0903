ed0cef0e84003ea775eddd681e7c1d49251fb648
Matheus Cirillo <matheuscirillo@usp.br>
2024-05-03 20:39:37 -0300

    updated python code
    works with only 1 run
    can handle minutes
    prints current run

--- scripts/daq.py
+++ scripts/daq.py
@@ -30,13 +30,21 @@ def compile():
 
     print("Code compiled successfully!")
 
+def clean_terminal():
+    print('\033[F', end='')
+    print('\033[K', end='')
+
+
 def run(runs, sizes, threads):
     response_times = {}
     # run the code
-    print("Starting data acquisition...")
+    print("Starting runs...")
     for _ in range(runs):
+        clean_terminal()
+        print("Run {}/{}".format(_+1, runs))
+        print("Running code...")
         for size in sizes:
-            print('\033[F', end='')
+            clean_terminal()
             print(f'Running with matrix of size {size} and sequentially')
             command = f'make run_seq ARGS="{size} {1} {0}"'
             result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -44,13 +52,14 @@ def run(runs, sizes, threads):
 
             time_pattern = r'real\s+(\d+m\d+\.\d+s)\nuser\s+(\d+m\d+\.\d+s)\nsys\s+(\d+m\d+\.\d+s)'
             match = re.search(time_pattern, output)
-            response_sequential = float(match.group(1).split('m')[1].split('s')[0])
+            response_sequential_minutes = float(match.group(1).split('m')[0])
+            response_sequential_seconds = float(match.group(1).split('m')[1].split('s')[0])
+            response_sequential = response_sequential_minutes * 60 + response_sequential_seconds
 
             response_times.setdefault(size, {}).setdefault(sequential, []).append(response_sequential)
 
             for thread in threads:
-                print('\033[F', end='')
-                print('\033[K', end='')
+                clean_terminal()
                 print(f'Running with matrix of size {size} and {thread} threads')
 
                 command = f'make run_par ARGS="{size} {thread} {0}"'
@@ -59,12 +68,14 @@ def run(runs, sizes, threads):
 
                 time_pattern = r'real\s+(\d+m\d+\.\d+s)\nuser\s+(\d+m\d+\.\d+s)\nsys\s+(\d+m\d+\.\d+s)'
                 match = re.search(time_pattern, output)
-                response_parallel = float(match.group(1).split('m')[1].split('s')[0])
+                response_parallel_minutes = float(match.group(1).split('m')[0])
+                response_parallel_seconds = float(match.group(1).split('m')[1].split('s')[0])
+                response_parallel = response_parallel_minutes * 60 + response_parallel_seconds
                 response_times[size].setdefault(parallel, {}).setdefault(thread, []).append(response_parallel)
+        clean_terminal()
 
     #clear entire last line in terminal
-    print('\033[F', end='')
-    print('\033[K', end='')
+    clean_terminal()
     print("Code ran successfully!")
     return response_times
 
@@ -76,7 +87,7 @@ def interpret_data(raw_data, show_data:bool=False):
             sequential_times = raw_data[size][sequential]
             sequential_median = round(statistics.median(sequential_times),5)
             sequential_mean = round(statistics.mean(sequential_times),5)
-            sequential_stdev = round(statistics.stdev(sequential_times),5)
+            sequential_stdev = round(statistics.stdev(sequential_times),5) if len(sequential_times) > 1 else 0
 
             for thread in raw_data[size][parallel]:
                 key = (size, thread)
@@ -90,14 +101,13 @@ def interpret_data(raw_data, show_data:bool=False):
                 data[key][parallel] = {}
                 data[key][parallel][median] = round(statistics.median(parallel_times),5)
                 data[key][parallel][mean] = round(statistics.mean(parallel_times),5)
-                data[key][parallel][stdev] = round(statistics.stdev(parallel_times),5)
+                data[key][parallel][stdev] = round(statistics.stdev(parallel_times),5) if len(parallel_times) > 1 else 0
 
                 data[key][speedup] = round(data[key][sequential][mean] / data[key][parallel][mean],5)
                 data[key][efficiency] = round(data[key][speedup] / key[1],5)
     except:
         exit('Error: could not interpret data')
-    print('\033[F', end='')
-    print('\033[K', end='')
+    clean_terminal()
     print("Data interpreted successfully!")
 
     if show_data:
@@ -120,13 +130,12 @@ def generate_csv(data, file_name='data.csv'):
                 file.write(f'{key[0]},{key[1]},{data[key][sequential][median]},{data[key][sequential][mean]},{data[key][sequential][stdev]},{data[key][parallel][median]},{data[key][parallel][mean]},{data[key][parallel][stdev]},{data[key][speedup]},{data[key][efficiency]}\n')
     except:
         exit('Error: could not write to file')
-    print('\033[F', end='')
-    print('\033[K', end='')
+    clean_terminal()
     print(f"CSV file successfuly generated at {file_path}!")
 
 compile()
-sizes = [2000,3000,4000]
-threads = [4, 8, 10]
+sizes = [50, 100, 500, 1000, 2000]
+threads = [4, 8, 12]
 runs = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 raw_data = run(runs, sizes, threads)
 data = interpret_data(raw_data, show_data=True)
