a67126ed4ca6d3f88bb30c2ed38bca0eed81c62f
guscarenci <gustavomourascarenci@usp.br>
2024-05-07 08:11:59 -0300

    adds few improvements on parallelism

--- src/jacobipar.c
+++ src/jacobipar.c
@@ -6,17 +6,12 @@
 
 float* jacobipar(float* matrix,float* constants,int N,float errorTolerance){
 
-    float* sum = malloc(sizeof(float)*N);
-    if (sum == NULL) {
-        fprintf(stderr, "Error: No sufficient memory!\n");
-        exit(1);
-    }
-
     float* lastVariables = malloc(sizeof(float)*N);
     if (lastVariables == NULL) {
         fprintf(stderr, "Error: No sufficient memory!\n");
         exit(1);
     }
+
     float* currentVariables = malloc(sizeof(float)*N);
     if (currentVariables == NULL) {
         fprintf(stderr, "Error: No sufficient memory!\n");
@@ -28,36 +23,28 @@ float* jacobipar(float* matrix,float* constants,int N,float errorTolerance){
         lastVariables[i] = constants[i];
     }
 
-    #pragma omp parallel for simd
-    for(int i = 0;i<N;i++){
-        sum[i] = 0;
-    }
-
-    float mr = 1;
-
     int stop = 0;
-
+    int count = 0;
     do {
-        #pragma omp parallel for simd
-        for(int i = 0;i<N;i++){
-            sum[i] = 0;
-        }
-        
-        float maxError = -1;
-        float maxVariable = -1;
 
-        #pragma omp parallel for shared(lastVariables,currentVariables,sum) collapse(2)
+        #pragma omp parallel for shared(lastVariables,currentVariables,matrix)
         for(int i = 0;i <N;i++){
+            float sum = 0;
+
+            #pragma omp simd reduction(+:sum)
             for(int j = 0; j < N; j++){
-                sum[i] += matrix[i * N + j] * lastVariables[j];
+                sum += matrix[i * N + j] * lastVariables[j];
             }
+            currentVariables[i] = (constants[i]-sum);
         }
 
-
-        #pragma omp task shared(stop)
+        #pragma omp task shared(stop) firstprivate(currentVariables, lastVariables)
         {
+            float mr = 1;
+            float maxError = -1;
+            float maxVariable = -1;
+            
             for(int i = 0;i<N;i++){
-                currentVariables[i] = (constants[i]-sum[i]);
                 float currentError = fabsf(currentVariables[i] - lastVariables[i]);
                 if (currentError > maxError) {
                     maxError = currentError;
@@ -67,6 +54,7 @@ float* jacobipar(float* matrix,float* constants,int N,float errorTolerance){
                     maxVariable = absCurrentVariable;
                 }
             }
+
             mr = maxError/maxVariable;
             stop = !(mr>errorTolerance);
         }
@@ -74,8 +62,11 @@ float* jacobipar(float* matrix,float* constants,int N,float errorTolerance){
         float* temp = lastVariables;
         lastVariables = currentVariables;
         currentVariables = temp;
+        count++;
 
     } while (!stop);
+
+    printf("Iterações: %d\n",count);
 
     free(currentVariables);
     return lastVariables; //Free outside
